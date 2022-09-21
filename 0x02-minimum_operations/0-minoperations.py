867b636e4e6218ed5788f1ab1fc89ebfe5f24d2e
3599josephw <3599@holbertonschool.com>
2022-09-21 17:10:49 -0500

    works up to 10 (exlcuding 8)

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -2,21 +2,36 @@
 """ minimum operations """
 
 
+from re import I
+
+
 def minOperations(n):
     """ calculates min ops to result in n H """
 
+    # Check is n is 1 or below
     if n <= 1:
         return 0
-
-    file = ['H']
-    count = len(file)
-    if isPrime(n):
+    # check if n is prime
+    i = isPrime(n)
+    if i is True:
         return n
+
+    # if not prime, calculate
+    largest = n / i
+    next = isPrime(largest)
+    if next is True:
+        if n % 2 == 0:
+            return int(largest + 2)
+        return int(largest + 3)
+
+
+
+# copy all > paste (2H) > copy all > paste (4h) > paste (6h) > paste (8h) paste (10h)
 
 
 def isPrime(n):
     """ checks if number is prime """
-    for i in range(2, int(n/2) + 1):
+    for i in range(2, int(n / 2) + 1):
         if (n % i) == 0:
-            return False
+            return i
     return True
