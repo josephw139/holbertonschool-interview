af5688b53969e852b92330fc7d49eb6fb365cbb5
3599josephw <3599@holbertonschool.com>
2022-12-08 10:34:09 -0600

    betty

--- 0x0A-menger/0-menger.c
+++ 0x0A-menger/0-menger.c
@@ -1,16 +1,32 @@
 #include "menger.h"
 
-void menger(int level) {
+/**
+ * menger - prints a menger sponge
+ * @level: level of sponge to print
+ */
+void menger(int level)
+{
 
 	int i, j, dim, d;
 
+	if (level < 0)
+	{
+		return;
+	}
+
 	for (i = 0, dim = 1; i < level; i++, dim *= 3);
 
-	for (i = 0; i < dim; i++) {
-		for (j = 0; j < dim; j++) {
+	for (i = 0; i < dim; i++)
+	{
+		for (j = 0; j < dim; j++)
+		{
 			for (d = dim / 3; d; d /= 3)
+			{
 				if ((i % (d * 3)) / d == 1 && (j % (d * 3)) / d == 1)
+				{
 					break;
+				}
+			}
 			printf(d ? " " : "#");
 		}
 		printf("\n");
