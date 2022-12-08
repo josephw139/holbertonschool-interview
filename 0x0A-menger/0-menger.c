67c1262d2204595f9e0e242d23a80831e909e21b
3599josephw <3599@holbertonschool.com>
2022-12-08 10:27:02 -0600

    sponge

---
+++ 0x0A-menger/0-menger.c
@@ -0,0 +1,18 @@
+#include "menger.h"
+
+void menger(int level) {
+
+	int i, j, dim, d;
+
+	for (i = 0, dim = 1; i < level; i++, dim *= 3);
+
+	for (i = 0; i < dim; i++) {
+		for (j = 0; j < dim; j++) {
+			for (d = dim / 3; d; d /= 3)
+				if ((i % (d * 3)) / d == 1 && (j % (d * 3)) / d == 1)
+					break;
+			printf(d ? " " : "#");
+		}
+		printf("\n");
+	}
+}
