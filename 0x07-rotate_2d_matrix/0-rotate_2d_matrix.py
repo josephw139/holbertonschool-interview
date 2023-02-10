b26d6019b69cb62f602e6dce6487f5568ba12017
3599josephw <3599@holbertonschool.com>
2023-02-10 11:01:34 -0600

    do a barrel roll

---
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -0,0 +1,16 @@
+import copy
+#!/usr/bin/python3
+"""
+0x07 - Rotate 2D Matrix
+"""
+
+
+def rotate_2d_matrix(matrix):
+    """ Do a barrel roll """
+    old_matrix = copy.deepcopy(matrix)
+
+    for row in range(len(matrix)):
+        col = len(matrix) - 1
+        for num in range(len(matrix[0])):
+            matrix[row][num] = old_matrix[col][row]
+            col -= 1
