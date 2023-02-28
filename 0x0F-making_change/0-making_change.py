2781652f2b90531f74c71c362635db437f261d9e
3599josephw <3599@holbertonschool.com>
2023-02-28 09:04:04 -0600

    pep8

--- 0x0F-making_change/0-making_change.py
+++ 0x0F-making_change/0-making_change.py
@@ -6,28 +6,27 @@ import sys
 
 
 def makeChange(coins, total):
-  """returns minimum # of coins to get total"""
-  if total <= 0:
-    return 0
+    """returns minimum # of coins to get total"""
+    if total <= 0:
+      return 0
 
-  length = len(coins)
+    length = len(coins)
 
-  table = [0 for i in range(total + 1)]
+    table = [0 for i in range(total + 1)]
 
-  table[0] = 0
+    table[0] = 0
 
-  for i in range(1, total + 1):
-    table[i] = sys.maxsize
+    for i in range(1, total + 1):
+      table[i] = sys.maxsize
 
 
-  for i in range(1, total + 1):
-    for j in range(length):
-      if (coins[j] <= i):
-        sub_res = table[i - coins[j]]
-        if (sub_res != sys.maxsize and
-            sub_res + 1 < table[i]):
+    for i in range(1, total + 1):
+      for j in range(length):
+        if (coins[j] <= i):
+          sub_res = table[i - coins[j]]
+          if (sub_res != sys.maxsize and sub_res + 1 < table[i]):
             table[i] = sub_res + 1
-  if table[total] == sys.maxsize:
-      return -1
+    if table[total] == sys.maxsize:
+        return -1
 
-  return table[total]
+    return table[total]
