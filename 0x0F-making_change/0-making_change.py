0891b971849c2826f6a88ac4d81faf0e739bfb2f
3599josephw <3599@holbertonschool.com>
2023-02-28 08:57:44 -0600

    task 0

---
+++ 0x0F-making_change/0-making_change.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+import sys
+
+
+def makeChange(coins, total):
+  if total <= 0:
+    return 0
+
+  length = len(coins)
+
+  table = [0 for i in range(total + 1)]
+
+  table[0] = 0
+
+  for i in range(1, total + 1):
+    table[i] = sys.maxsize
+
+
+  for i in range(1, total + 1):
+    for j in range(length):
+      if (coins[j] <= i):
+        sub_res = table[i - coins[j]]
+        if (sub_res != sys.maxsize and
+            sub_res + 1 < table[i]):
+            table[i] = sub_res + 1
+  if table[total] == sys.maxsize:
+      return -1
+
+  return table[total]
