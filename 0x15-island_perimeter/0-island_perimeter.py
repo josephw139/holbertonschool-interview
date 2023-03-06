ffeb86cc595ed151837c312c0a958f40d296e301
3599josephw <3599@holbertonschool.com>
2023-03-06 11:30:57 -0600

    island perimeter

---
+++ 0x15-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+
+def island_perimeter(grid):
+    """Calculates perimeter around binary islands"""
+    perimeter = 0
+    for row in range(len(grid)):
+        for i in range(len(grid[0])):
+            if grid[row][i] == 1:
+                # check above
+                if grid[row - 1][i] == 0:
+                    perimeter += 1
+                #check left
+                if grid[row][i - 1] == 0:
+                    perimeter += 1
+                #check right
+                if grid[row][i + 1] == 0:
+                    perimeter += 1
+                #check below
+                if grid[row + 1][i] == 0:
+                    perimeter += 1
+
+    return perimeter
